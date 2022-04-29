ba3a6e51e321e861672224a064a1bfe62b707d3f
Andrés Moschini <andresmoschini@gmail.com>
2022-04-29 08:51:48 -0300

    greeter(step 4): return `Good morning <name>` when the time is 06:00-12:00

--- src/andresmoschini/Greeter.test.ts
+++ src/andresmoschini/Greeter.test.ts
@@ -1,20 +1,46 @@
 import { Greeter } from "./Greeter";
 
 describe("Greeter.greet", () => {
-  it("should return a message starting with Hello", () => {
-    // arrange
-    const greeter = new Greeter();
+  it.each([
+    {
+      currentTime: "2022-04-27T05:59:59.999Z",
+      expectedPrefix: "Hello",
+    },
+    {
+      currentTime: "2022-04-27T06:00:00.000Z",
+      expectedPrefix: "Good morning",
+    },
+    {
+      currentTime: "2022-04-27T11:59:59.999Z",
+      expectedPrefix: "Good morning",
+    },
+    {
+      currentTime: "2022-04-27T12:00:00.000Z",
+      expectedPrefix: "Hello",
+    },
+    {
+      currentTime: "2022-04-27T22:00:00.000Z",
+      expectedPrefix: "Hello",
+    },
+  ])(
+    "should return a message starting with $expectedPrefix when currentTime is $currentTime",
+    ({ currentTime, expectedPrefix }) => {
+      // arrange
+      const getCurrentDateTime = () => new Date(currentTime);
+      const greeter = new Greeter({ getCurrentDateTime });
 
-    // act
-    const result = greeter.greet("World");
+      // act
+      const result = greeter.greet("World");
 
-    // assert
-    expect(result).toMatch(/^Hello/);
-  });
+      // assert
+      expect(result).toMatch(new RegExp(`^${expectedPrefix}`));
+    }
+  );
 
   it("should return a message including the name", () => {
     // arrange
-    const greeter = new Greeter();
+    const getCurrentDateTime = () => new Date();
+    const greeter = new Greeter({ getCurrentDateTime });
 
     // act
     const result = greeter.greet("World");
@@ -25,7 +51,8 @@ describe("Greeter.greet", () => {
 
   it("should return a message saying `Hello <name>`", () => {
     // arrange
-    const greeter = new Greeter();
+    const getCurrentDateTime = () => new Date("2022-04-27T22:00:00.000Z");
+    const greeter = new Greeter({ getCurrentDateTime });
 
     // act
     const result = greeter.greet("World");
@@ -43,7 +70,8 @@ describe("Greeter.greet", () => {
     "should return a message trimming extra spaces in the name (name: `$name`)",
     ({ name }) => {
       // arrange
-      const greeter = new Greeter();
+      const getCurrentDateTime = () => new Date("2022-04-27T22:00:00.000Z");
+      const greeter = new Greeter({ getCurrentDateTime });
 
       // act
       const result = greeter.greet(name);
@@ -57,7 +85,8 @@ describe("Greeter.greet", () => {
     "should return `Hello ` when name is white spaces or empty string (name: `$name`)",
     ({ name }) => {
       // arrange
-      const greeter = new Greeter();
+      const getCurrentDateTime = () => new Date("2022-04-27T22:00:00.000Z");
+      const greeter = new Greeter({ getCurrentDateTime });
 
       // act
       const result = greeter.greet(name);
@@ -78,7 +107,8 @@ describe("Greeter.greet", () => {
     "should return a message capitalizing the first letter of the name (name: `$name`)",
     ({ name, expectedName }) => {
       // arrange
-      const greeter = new Greeter();
+      const getCurrentDateTime = () => new Date("2022-04-27T22:00:00.000Z");
+      const greeter = new Greeter({ getCurrentDateTime });
 
       // act
       const result = greeter.greet(name);
--- src/andresmoschini/Greeter.ts
+++ src/andresmoschini/Greeter.ts
@@ -1,10 +1,25 @@
+interface GreeterDependencies {
+  getCurrentDateTime: () => Date;
+}
+
 export class Greeter {
-  constructor() {}
+  private readonly _getCurrentDateTime: () => Date;
+
+  constructor({ getCurrentDateTime }: GreeterDependencies) {
+    this._getCurrentDateTime = getCurrentDateTime;
+  }
 
   greet(name: string) {
     const trimmed = name.trim();
     const first = trimmed.slice(0, 1);
     const rest = trimmed.slice(1);
-    return `Hello ${first.toUpperCase()}${rest}`;
+    const sanitizedName = `${first.toUpperCase()}${rest}`;
+
+    const currentTime = this._getCurrentDateTime();
+    const currentHour = currentTime.getUTCHours();
+    const prefix =
+      currentHour >= 6 && currentHour < 12 ? "Good morning" : "Hello";
+
+    return `${prefix} ${sanitizedName}`;
   }
 }
