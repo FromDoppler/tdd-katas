b8cfdc078662cb7744edff1fe16627a1ffa3e404
Cromeror <cristobalromerorossi@gmail.com>
2022-05-09 09:46:33 -0500

    greeter kata step 2 - test

---
+++ src/cromeror/Greeter.test.ts
@@ -0,0 +1,29 @@
+import { Greeter } from "./Greeter";
+
+describe("Greeter.greet", () => {
+  it("should return a message with Hello name", () => {
+    // arrange
+    const greeter = new Greeter();
+
+    // act
+    const result = greeter.greet("Cromeror");
+
+    // assert
+    expect(result).toBe("Hello Cromeror");
+  });
+
+  it
+    .each([
+      "Cromeror   ", "   Cromeror", "   Cromeror   "
+    ])
+    ("should return a trim message with Hello Cromeror", (name) => {
+      // arrange
+      const greeter = new Greeter();
+      const trimRegex = /(^\S+[\S\s]+\S+$)|(^\S$)/;
+      // act
+      const result = greeter.greet(name);
+
+      // assert
+      expect(result).toMatch(trimRegex);
+    });
+});
