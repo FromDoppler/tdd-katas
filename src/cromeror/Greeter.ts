4e983059193fc84be9facea3bf920644bbc15301
Cromeror <cristobalromerorossi@gmail.com>
2022-05-09 10:13:06 -0500

    greeter kata step 3 - implementation

--- src/cromeror/Greeter.ts
+++ src/cromeror/Greeter.ts
@@ -2,6 +2,7 @@ export class Greeter {
   constructor() {}
 
   greet(name: string) {
+    name = name.charAt(0).toUpperCase() + name.toLowerCase().slice(1);
     return `Hello ${name.trim()}`;
   }
 }
