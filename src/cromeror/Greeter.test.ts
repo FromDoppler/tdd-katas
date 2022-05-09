2cc1a1d0d4c289da077a9aed30b1c28d20f3aad4
Cromeror <cristobalromerorossi@gmail.com>
2022-05-09 10:25:23 -0500

    greeter kata step 4 - test

--- src/cromeror/Greeter.test.ts
+++ src/cromeror/Greeter.test.ts
@@ -3,8 +3,7 @@ import { Greeter } from "./Greeter";
 describe("Greeter.greet", () => {
   it("should return a message with Hello name", () => {
     // arrange
-    const greeter = new Greeter();
-
+    const greeter = new Greeter({ getTime: () => 0 });
     // act
     const result = greeter.greet("Cromeror");
 
@@ -18,7 +17,7 @@ describe("Greeter.greet", () => {
     ])
     ("should return a trim message with Hello Cromeror", (name) => {
       // arrange
-      const greeter = new Greeter();
+      const greeter = new Greeter({ getTime: () => 0 });
       const trimRegex = /(^\S+[\S\s]+\S+$)|(^\S$)/;
       // act
       const result = greeter.greet(name);
@@ -31,10 +30,21 @@ describe("Greeter.greet", () => {
     .each(["cromeror", "cromEror"])
     ("should return a greeting message with capitalize name", (name) => {
       // arrange
-      const greeter = new Greeter();
+      const greeter = new Greeter({ getTime: () => 0 });
       // act
       const result = greeter.greet(name);
       // assert
       expect(result).toBe("Hello Cromeror");
+    });
+
+  it
+    .each([6, 8, 12])
+    ("should return a message with Good morning Cromeror", (time) => {
+      // arrange
+      const greeter = new Greeter({ getTime: () => time });
+      // act
+      const result = greeter.greet("Cromeror");
+      // assert
+      expect(result).toBe("Good morning Cromeror");
     });
 });
--- src/cromeror/Greeter.ts
+++ src/cromeror/Greeter.ts
@@ -1,5 +1,13 @@
+interface Time {
+  getTime: () => number;
+}
+
 export class Greeter {
-  constructor() {}
+  private time: Time;
+
+  constructor(time: Time) {
+    this.time = time;
+  }
 
   greet(name: string) {
     name = name.charAt(0).toUpperCase() + name.toLowerCase().slice(1);
