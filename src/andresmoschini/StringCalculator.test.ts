c873771942ffa0fdb04eb2b28a8164bafe2780e6
Andrés Moschini <andresmoschini@gmail.com>
2022-05-14 16:00:05 -0300

    string-calculator(step 3): return the sum of comma delimited values

--- src/andresmoschini/StringCalculator.test.ts
+++ src/andresmoschini/StringCalculator.test.ts
@@ -14,12 +14,7 @@ describe("StringCalculator.add", () => {
     expect(result).toEqual(expectedResult);
   });
 
-  it.each([
-    { input: "   " },
-    { input: "abc" },
-    { input: ",5,6,7" },
-    { input: "x5x6x7x" },
-  ])(
+  it.each([{ input: "   " }, { input: "abc" }, { input: "x5x6x7x" }])(
     "should return NaN when the input is not an expected value ($input)",
     ({ input }) => {
       // arrange
@@ -56,13 +51,32 @@ describe("StringCalculator.add", () => {
     { input: 0, expectedResult: 0 },
     { input: 567, expectedResult: 567 },
     { input: "567", expectedResult: 567 },
-    { input: "5,6,7", expectedResult: 5 },
     { input: "5x6x7x", expectedResult: 5 },
     { input: "5\n6\n7\n", expectedResult: 5 },
     { input: " 567 ", expectedResult: 567 },
     { input: "\n5\n6\n7\n", expectedResult: 5 },
   ])(
     "should return the value when the input is parsable as number ($input)",
+    ({ input, expectedResult }) => {
+      // arrange
+      const stringCalculator = new StringCalculator();
+
+      // act
+      const result = stringCalculator.add(input as string);
+
+      // assert
+      expect(result).toEqual(expectedResult);
+    }
+  );
+
+  it.each([
+    { input: "1,2", expectedResult: 3 },
+    { input: "10,20", expectedResult: 30 },
+    { input: "5,6,7", expectedResult: 18 },
+    { input: ",5,6,7", expectedResult: 18 },
+    { input: ",,,", expectedResult: 0 },
+  ])(
+    "should return the sum for comma delimited values ($input)",
     ({ input, expectedResult }) => {
       // arrange
       const stringCalculator = new StringCalculator();
--- src/andresmoschini/StringCalculator.ts
+++ src/andresmoschini/StringCalculator.ts
@@ -6,6 +6,20 @@ export class StringCalculator {
       return 0;
     }
 
-    return parseInt(input);
+    if (typeof input === "number") {
+      return input;
+    }
+
+    if (typeof input !== "string") {
+      return NaN;
+    }
+
+    const items = input.split(",");
+
+    if (items.length === 1) {
+      return parseInt(input);
+    }
+
+    return items.reduce((acc, cur) => acc + this.add(cur), 0);
   }
 }
