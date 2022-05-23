5a25fc5479e943f4aeed08a523c7d44e187e1097
Cromeror <cristobalromerorossi@gmail.com>
2022-05-23 08:58:46 -0500

    stringCalculator impl - step 7

--- src/cromero/StringCalculator.ts
+++ src/cromero/StringCalculator.ts
@@ -4,6 +4,9 @@ export class StringCalculator {
     let sum = 0;
     matches && matches.forEach((n: string) => {
       const nAsNumber = parseInt(n);
+      if(nAsNumber > 1000){
+        return;
+      }
       if (nAsNumber < 0) {
         throw new Error("The string contains negatives numbers");
       }
