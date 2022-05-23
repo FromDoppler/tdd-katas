b915bd26f6c42340a280beac89060a9e0606c8ca
Cromeror <cristobalromerorossi@gmail.com>
2022-05-23 08:43:44 -0500

    stringCalculator test - step 5

--- src/cromero/StringCalculator.test.ts
+++ src/cromero/StringCalculator.test.ts
@@ -17,7 +17,8 @@ describe("StringCalculator.add", () => {
     { input: "2", sum: 2 },
     { input: "1,2", sum: 3 },
     { input: "10,20", sum: 30 },
-    { input: "1\\n2", sum: 3 }
+    { input: "1\\n2", sum: 3 },
+    { input: "1\\n2,3\\n4", sum: 10 }
   ])(`should return $input of $sum string`, ({ input, sum }) => {
     // arrange
     const stringCalculator = new StringCalculator();
