dd9602260bac4cedfe0264f7ccdd6108f8319392
Andrés Moschini <andresmoschini@gmail.com>
2022-06-02 17:22:56 -0300

    prime-factors(step 4): refactor tests in a single table

--- src/andresmoschini/PrimeFactors.test.ts
+++ src/andresmoschini/PrimeFactors.test.ts
@@ -3,90 +3,68 @@ import { generate } from "./PrimeFactors";
 describe(generate.name, () => {
   it.each([
     {
-      n: -1,
+      scenario: "supported value",
+      n: 1,
+      expectedResult: [],
     },
     {
-      n: -2,
+      scenario: "supported value",
+      n: 2,
+      expectedResult: [2],
     },
     {
-      n: null,
+      scenario: "supported value",
+      n: 3,
+      expectedResult: [3],
     },
     {
-      n: undefined,
+      scenario: "supported value",
+      n: 4,
+      expectedResult: [2, 2],
     },
     {
-      n: NaN,
-    },
-    {
-      n: "hola",
-    },
-  ])(
-    "should return an empty array when n is $n (a not a supported type)",
-    ({ n }: any) => {
-      // Act
-      const result = generate(n);
-
-      // Assert
-      expect(result).toEqual([]);
-    }
-  );
-
-  it.each([
-    {
+      scenario: "unsupported value yet",
       n: 1234568789,
+      expectedResult: [1234568789],
+    },
+    {
+      scenario: "unsupported type",
+      n: -1,
+      expectedResult: [],
+    },
+    {
+      scenario: "unsupported type",
+      n: -2,
+      expectedResult: [],
+    },
+    {
+      scenario: "unsupported type",
+      n: null,
+      expectedResult: [],
+    },
+    {
+      scenario: "unsupported type",
+      n: undefined,
+      expectedResult: [],
+    },
+    {
+      scenario: "unsupported type",
+      n: NaN,
+      expectedResult: [],
+    },
+    {
+      scenario: "unsupported type",
+      n: "hola",
+      expectedResult: [],
     },
   ])(
-    "should return an array with one n element when n is $n (a not a supported value yet)",
-    ({ n }: any) => {
+    "should return $expectedResult when n is $n ($scenario)",
+    ({ n, expectedResult }: any) => {
       // Act
       const result = generate(n);
 
       // Assert
-      expect(result).toEqual([n]);
+      expect(result).toEqual(expectedResult);
     }
   );
-
-  it("should return [] when n is 1", () => {
-    // Arrange
-    const n = 1;
-
-    // Act
-    const result = generate(n);
-
-    // Assert
-    expect(result).toEqual([]);
-  });
-
-  it("should return [2] when n is 2", () => {
-    // Arrange
-    const n = 2;
-
-    // Act
-    const result = generate(n);
-
-    // Assert
-    expect(result).toEqual([2]);
-  });
-
-  it("should return [3] when n is 3", () => {
-    // Arrange
-    const n = 3;
-
-    // Act
-    const result = generate(n);
-
-    // Assert
-    expect(result).toEqual([3]);
-  });
-
-  it("should return [2,2] when n is 4", () => {
-    // Arrange
-    const n = 4;
-
-    // Act
-    const result = generate(n);
-
-    // Assert
-    expect(result).toEqual([2, 2]);
-  });
 });
