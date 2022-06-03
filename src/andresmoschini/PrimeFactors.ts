d3c8b74e620ba7ff7179d12bc28c0256c7c78225
Andrés Moschini <andresmoschini@gmail.com>
2022-06-03 08:55:39 -0300

    prime-factors(complete): optimize to start searching from the last found factor
    Based on Uncle Bob solution review: http://butunclebob.com/ArticleS.UncleBob.ThePrimeFactorsKata

--- src/andresmoschini/PrimeFactors.ts
+++ src/andresmoschini/PrimeFactors.ts
@@ -2,19 +2,20 @@ export function generate(n: number) {
   const factors = [];
 
   while (n > 1) {
-    const factor = searchOneFactor(n);
+    let nextCandidate = 2;
+    const factor = searchOneFactor({ n, startFrom: nextCandidate });
     factors.push(factor);
     n = n / factor;
+    nextCandidate = factor;
   }
 
   return factors;
 }
 
-function searchOneFactor(n: number) {
-  let candidate = 2;
+function searchOneFactor({ n, startFrom }: { n: number; startFrom: number }) {
+  let candidate = startFrom;
   while (n % candidate !== 0) {
     candidate++;
   }
   return candidate;
 }
-
