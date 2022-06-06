33dbcc842654eb8c1703d9e2857648b3cddf8b34
Cromeror <cristobalromerorossi@gmail.com>
2022-06-06 14:30:55 -0500

    apply format

--- src/cromero/PrimeFactor.test.ts
+++ src/cromero/PrimeFactor.test.ts
@@ -1,21 +1,22 @@
 import { generate } from "./PrimeFactor";
 
 describe("PrimeFactors.generate", () => {
-  it
-    .each([
-      { number: 1, expected: [] },
-      { number: 2, expected: [2] },
-      { number: 3, expected: [3] },
-      { number: 4, expected: [2, 2] },
-      { number: 5, expected: [5] },
-      { number: 6, expected: [2, 3] },
-      { number: 7, expected: [7] },
-      { number: 8, expected: [2, 2, 2] },
-      { number: 9, expected: [3, 3] },
-      { number: 4620, expected: [2, 2, 3, 5, 7, 11] }
-    ])
-    ("should return $expected when the number is $number", ({ number, expected }) => {
+  it.each([
+    { number: 1, expected: [] },
+    { number: 2, expected: [2] },
+    { number: 3, expected: [3] },
+    { number: 4, expected: [2, 2] },
+    { number: 5, expected: [5] },
+    { number: 6, expected: [2, 3] },
+    { number: 7, expected: [7] },
+    { number: 8, expected: [2, 2, 2] },
+    { number: 9, expected: [3, 3] },
+    { number: 4620, expected: [2, 2, 3, 5, 7, 11] },
+  ])(
+    "should return $expected when the number is $number",
+    ({ number, expected }) => {
       const primeFactors = generate(number);
       expect(primeFactors).toEqual(expected);
-    });
+    }
+  );
 });
