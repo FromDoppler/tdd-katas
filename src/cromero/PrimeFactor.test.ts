ad4bce47114904a06207c107d2954fc6e7d571e3
Cromeror <cristobalromerorossi@gmail.com>
2022-06-06 11:31:26 -0500

    prime factors - step 3 test

--- src/cromero/PrimeFactor.test.ts
+++ src/cromero/PrimeFactor.test.ts
@@ -4,7 +4,8 @@ describe("PrimeFactors.generate", () => {
   it
     .each([
       { number: 1, expected: [] },
-      { number: 2, expected: [2] }
+      { number: 2, expected: [2] },
+      { number: 3, expected: [3] }
     ])
     ("should return $expected when the number is $number", ({ number, expected }) => {
       const primeFactors = generate(number);
