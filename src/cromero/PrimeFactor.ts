a9212bc9d857b7dcee51796693ab9b9ab444c964
Cromeror <cristobalromerorossi@gmail.com>
2022-06-06 11:31:34 -0500

    prime factors - step 3 impl

--- src/cromero/PrimeFactor.ts
+++ src/cromero/PrimeFactor.ts
@@ -1,8 +1,25 @@
 export const generate = (input: number) => {
-  switch (input) {
-    case 1:
-      return [];
-    case 2:
-      return [2];
+  if (input === 1) {
+    return [];
   }
+  if (input === 2) {
+    return [2];
+  }
+  if (isPrime(input)) {
+    return [input];
+  }
+};
+
+const isPrime = (integer: number): boolean => {
+  const multiples = [];
+  if (integer === 1 || integer === 2) {
+    return false;
+  }
+
+  for (let i = 1; i <= integer; i++) {
+    if (integer % i === 0) {
+      multiples.push(i);
+    }
+  }
+  return multiples.length === 2;
 };
