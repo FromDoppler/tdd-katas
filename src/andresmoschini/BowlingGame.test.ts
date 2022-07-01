29fa7463f084fe9ef8c81e6ff47243b0dc21c3f6
Andrés Moschini <andresmoschini@gmail.com>
2022-07-01 10:32:12 -0300

    feat(bowling): support gutter game

--- src/andresmoschini/BowlingGame.test.ts
+++ src/andresmoschini/BowlingGame.test.ts
@@ -12,4 +12,25 @@ describe(BowlingGame.name, () => {
       expect(sut[functionName]).toBeInstanceOf(Function);
     }
   );
+
+  it("should return 0 for a gutter game", () => {
+    // Arrange
+    const game = new BowlingGame();
+    const rollsQty = 20;
+    const gutterPines = 0;
+    const expectedScore = 0;
+
+    // Act
+    range(rollsQty).forEach((i) => {
+      game.roll(gutterPines);
+    });
+
+    // Assert
+    expect(game.getScore()).toBe(expectedScore);
+  });
 });
+
+
+function range(size: number, startAt = 0) {
+  return [...Array(size).keys()].map((i) => i + startAt);
+}
