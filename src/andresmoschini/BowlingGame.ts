9a30df76875ffb30f15153cb536d457fac5b71c6
Andres Moschini <andresmoschini@gmail.com>
2022-07-03 19:39:17 -0300

    refactor: function to calculate frame score

--- src/andresmoschini/BowlingGame.ts
+++ src/andresmoschini/BowlingGame.ts
@@ -1,4 +1,4 @@
-import { range } from "./utils";
+import { range, sum } from "./utils";
 
 const TOTAL_PINS = 10;
 const FRAMES_IN_A_GAME = 10;
@@ -12,22 +12,24 @@ export class BowlingGame {
     this.rolls[this.currentRoll++] = pins;
   }
 
-  getScore(): number {
-    let score = 0;
-    range(FRAMES_IN_A_GAME).forEach((frameIndex) => {
-      const roll1Index = frameIndex * 2;
-      const roll2Index = roll1Index + 1;
-      const nextFrameRoll1Index = roll2Index + 1;
-      const isSpare =
-        this.rolls[roll1Index] + this.rolls[roll2Index] === TOTAL_PINS;
-      const frameScore = isSpare
-        ? this.rolls[roll1Index] +
+  getFrameScore(frameIndex: number) {
+    const roll1Index = frameIndex * 2;
+    const roll2Index = roll1Index + 1;
+    const nextFrameRoll1Index = roll2Index + 1;
+    const isSpare =
+      this.rolls[roll1Index] + this.rolls[roll2Index] === TOTAL_PINS;
+    return isSpare
+      ? this.rolls[roll1Index] +
           this.rolls[roll2Index] +
           this.rolls[nextFrameRoll1Index]
-        : this.rolls[roll1Index] + this.rolls[roll2Index];
+      : this.rolls[roll1Index] + this.rolls[roll2Index];
+  }
 
-      score += frameScore;
-    });
-    return score;
+  getScore(): number {
+    return sum(
+      range(FRAMES_IN_A_GAME).map((frameIndex) =>
+        this.getFrameScore(frameIndex)
+      )
+    );
   }
 }
