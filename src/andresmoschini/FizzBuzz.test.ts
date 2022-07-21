79670e351d49de2f72f4eca6c5b28e4f05cc2ac4
Andres Moschini <andresmoschini@gmail.com>
2022-07-21 17:47:14 -0300

    feat(fizzbuzz): return "Buzz" for multiples of 5 and "FizzBuzz" for multiples of 15

--- src/andresmoschini/FizzBuzz.test.ts
+++ src/andresmoschini/FizzBuzz.test.ts
@@ -28,15 +28,21 @@ describe("FizzBuzz.generate", () => {
   it.each([
     { position: 1, expectedValue: "1" },
     { position: 3, expectedValue: "Fizz" },
+    { position: 5, expectedValue: "Buzz" },
     { position: 6, expectedValue: "Fizz" },
     { position: 9, expectedValue: "Fizz" },
+    { position: 10, expectedValue: "Buzz" },
     { position: 12, expectedValue: "Fizz" },
-    { position: 15, expectedValue: "Fizz" },
+    { position: 15, expectedValue: "FizzBuzz" },
     { position: 18, expectedValue: "Fizz" },
     { position: 19, expectedValue: "19" },
+    { position: 30, expectedValue: "FizzBuzz" },
+    { position: 45, expectedValue: "FizzBuzz" },
+    { position: 90, expectedValue: "FizzBuzz" },
+    { position: 95, expectedValue: "Buzz" },
     { position: 98, expectedValue: "98" },
     { position: 99, expectedValue: "Fizz" },
-    { position: 100, expectedValue: "100" },
+    { position: 100, expectedValue: "Buzz" },
   ])(
     "should return $expectedValue in the $position position",
     ({ position, expectedValue }) => {
--- src/andresmoschini/FizzBuzz.ts
+++ src/andresmoschini/FizzBuzz.ts
@@ -2,6 +2,14 @@ import { range } from "./utils";
 
 export class FizzBuzz {
   generate(): string[] {
-    return range(100, 1).map((i) => (i % 3 == 0 ? "Fizz" : i.toString()));
+    return range(100, 1).map((i) =>
+      i % 15 == 0
+        ? "FizzBuzz"
+        : i % 5 == 0
+        ? "Buzz"
+        : i % 3 == 0
+        ? "Fizz"
+        : i.toString()
+    );
   }
 }
