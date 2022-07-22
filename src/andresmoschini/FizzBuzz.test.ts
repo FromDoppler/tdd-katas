6ec3d9d8646d13ca6d006dfb8d6831f916b0bf2b
Andres Moschini <andresmoschini@gmail.com>
2022-07-22 09:25:38 -0300

    feat(fizzbuzz): allow to configure start and end

--- src/andresmoschini/FizzBuzz.test.ts
+++ src/andresmoschini/FizzBuzz.test.ts
@@ -26,6 +26,62 @@ describe("FizzBuzz.generate", () => {
   });
 
   it.each([
+    {
+      start: undefined,
+      end: undefined,
+      expectedLength: 100,
+      expectedFirstItem: "1",
+      expectedLastItem: "Buzz",
+    },
+    {
+      start: 1,
+      end: 20,
+      expectedLength: 20,
+      expectedFirstItem: "1",
+      expectedLastItem: "Buzz",
+    },
+    {
+      start: 15,
+      end: 50,
+      expectedLength: 36,
+      expectedFirstItem: "FizzBuzz",
+      expectedLastItem: "Buzz",
+    },
+    {
+      start: 18,
+      end: 49,
+      expectedLength: 32,
+      expectedFirstItem: "Fizz",
+      expectedLastItem: "49",
+    },
+    {
+      start: 19,
+      end: 48,
+      expectedLength: 30,
+      expectedFirstItem: "19",
+      expectedLastItem: "Fizz",
+    },
+  ])(
+    "should return an array with $expectedLength elements depending on the $start and $end values",
+    ({ start, end, expectedLength, expectedFirstItem, expectedLastItem }) => {
+      // Arrange
+      const sut = new FizzBuzz({ start, end });
+
+      // Act
+      const result = sut.generate();
+
+      // Assert
+      expect(result).toBeInstanceOf(Array);
+      expect(result).toHaveLength(expectedLength);
+      expect(result[0]).toBe(expectedFirstItem);
+      for (const resultItem in result) {
+        expect(typeof resultItem).toBe("string");
+      }
+      expect(result.pop()).toBe(expectedLastItem);
+    }
+  );
+
+  it.each([
     { position: 1, expectedValue: "1" },
     { position: 3, expectedValue: "Fizz" },
     { position: 5, expectedValue: "Buzz" },
--- src/andresmoschini/FizzBuzz.ts
+++ src/andresmoschini/FizzBuzz.ts
@@ -1,8 +1,16 @@
 import { range } from "./utils";
 
 export class FizzBuzz {
+  private readonly start: number;
+  private readonly end: number;
+
+  constructor(options: { start?: number; end?: number } = {}) {
+    this.start = options.start || 1;
+    this.end = options.end || 100;
+  }
+
   generate(): string[] {
-    return range(100, 1).map((i) =>
+    return range(this.end - this.start + 1, this.start).map((i) =>
       i % 15 == 0
         ? "FizzBuzz"
         : i % 5 == 0
