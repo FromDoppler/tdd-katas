c8e6036cc72df64619b9f7ebd8c902e970781d10
Andres Moschini <andresmoschini@gmail.com>
2023-04-14 15:16:29 -0300

    build: remove symbolic link for Jenkinsfile

--- doppler-jenkins-ci.groovy
+++ doppler-jenkins-ci.groovy
@@ -1,1 +1,25 @@
-Jenkinsfile
+pipeline {
+    agent any
+    stages {
+        stage('Verify .sh files') {
+            steps {
+                sh 'docker build --target verify-sh .'
+            }
+        }
+        stage('Restore') {
+            steps {
+                sh 'docker build --target restore .'
+            }
+        }
+        stage('Verify Format') {
+            steps {
+                sh 'docker build --target verify-format .'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'docker build --target test .'
+            }
+        }
+    }
+}
